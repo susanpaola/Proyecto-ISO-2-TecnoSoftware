395746dee93d7572214c00f70330266c102c2ff1
susan <susanpaola.herrera@alu.uclm.es>
2022-12-25 14:10:20 +0100

    get y set entidad profesor

--- Proyecto_TecnoSoftware/src/negocio/entities/Profesor.java
+++ Proyecto_TecnoSoftware/src/negocio/entities/Profesor.java
@@ -6,5 +6,37 @@ public class Profesor {
 	private String nombre;
 	private String apellidos;
 	private boolean doctor;
+	
+	public Profesor(String dni, String nombre, String apellidos, boolean doctor) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.doctor = doctor;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public boolean isDoctor() {
+		return doctor;
+	}
+	public void setDoctor(boolean doctor) {
+		this.doctor = doctor;
+	}
 
 }
