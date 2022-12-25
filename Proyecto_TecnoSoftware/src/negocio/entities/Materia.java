a56f863a8e791bac36ebb7f42be4bd95a879b820
susan <susanpaola.herrera@alu.uclm.es>
2022-12-25 14:08:54 +0100

    completacion de entidades materia y matricula

--- Proyecto_TecnoSoftware/src/negocio/entities/Materia.java
+++ Proyecto_TecnoSoftware/src/negocio/entities/Materia.java
@@ -9,5 +9,46 @@ public class Materia {
 	private double horas;
 	private Date fechaInicio;
 	private Date fechaFin;
+	
+	public Materia(Profesor responsable, String nombre, double horas, Date fechaInicio, Date fechaFin) {
+		super();
+		this.responsable = responsable;
+		this.nombre = nombre;
+		this.horas = horas;
+		this.fechaInicio = fechaInicio;
+		this.fechaFin = fechaFin;
+	}
+	public Profesor getResponsable() {
+		return responsable;
+	}
+	public void setResponsable(Profesor responsable) {
+		this.responsable = responsable;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getHoras() {
+		return horas;
+	}
+	public void setHoras(double horas) {
+		this.horas = horas;
+	}
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+	
+	
 
 }
--- Proyecto_TecnoSoftware/src/negocio/entities/Matricula.java
+++ Proyecto_TecnoSoftware/src/negocio/entities/Matricula.java
@@ -10,5 +10,52 @@ public class Matricula {
 	private Date fecha;
 	private boolean pagado;
 	private int attribute;
-
+	
+	public Matricula(Estudiante estudiante, CursoPropio titulo, ModoPago tipoPago, Date fecha, boolean pagado,
+			int attribute) {
+		super();
+		this.estudiante = estudiante;
+		this.titulo = titulo;
+		this.tipoPago = tipoPago;
+		this.fecha = fecha;
+		this.pagado = pagado;
+		this.attribute = attribute;
+	}
+	public Estudiante getEstudiante() {
+		return estudiante;
+	}
+	public void setEstudiante(Estudiante estudiante) {
+		this.estudiante = estudiante;
+	}
+	public CursoPropio getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(CursoPropio titulo) {
+		this.titulo = titulo;
+	}
+	public ModoPago getTipoPago() {
+		return tipoPago;
+	}
+	public void setTipoPago(ModoPago tipoPago) {
+		this.tipoPago = tipoPago;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public boolean isPagado() {
+		return pagado;
+	}
+	public void setPagado(boolean pagado) {
+		this.pagado = pagado;
+	}
+	public int getAttribute() {
+		return attribute;
+	}
+	public void setAttribute(int attribute) {
+		this.attribute = attribute;
+	}
+	
 }
