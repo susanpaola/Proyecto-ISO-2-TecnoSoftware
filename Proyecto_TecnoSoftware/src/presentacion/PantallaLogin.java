3df1b694df6248ed2de679380dcf7dcf0dcaad89
susan <susanpaola.herrera@alu.uclm.es>
2022-12-26 14:47:09 +0100

    pantallas login y Menu pre-finalizadas

--- Proyecto_TecnoSoftware/src/presentacion/PantallaLogin.java
+++ Proyecto_TecnoSoftware/src/presentacion/PantallaLogin.java
@@ -16,6 +16,7 @@ import javax.swing.JTextField;
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.EventQueue;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -59,8 +60,6 @@ public class PantallaLogin  {
 	}
 
 	private static void placeComponents(JPanel panel) {
-
-		panel.setLayout(null);
 	
 	}
 
@@ -71,7 +70,7 @@ public class PantallaLogin  {
 			JFrame frmUclm = new JFrame("Demo application");
 			frmUclm.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Usuario\\git\\PROYECTOS_GIT\\TecnoSoftware\\Proyecto-ISO-2-TecnoSoftware\\Proyecto_TecnoSoftware\\Imagenes\\IconUCLM.png"));
 			frmUclm.setTitle("UCLM");
-			frmUclm.setSize(625, 428);
+			frmUclm.setSize(655, 467);
 			frmUclm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 			JPanel panel = new JPanel();
@@ -79,41 +78,43 @@ public class PantallaLogin  {
 			frmUclm.getContentPane().add(panel);
 			placeComponents(panel);
 			
-			JButton loginButton = new JButton("Iniciar sesi\u00F3n");
+			JButton loginButton = new JButton("Iniciar secion");
+			loginButton.setBounds(406, 316, 168, 48);
 			loginButton.hide();
-			loginButton.setForeground(Color.WHITE);
+			panel.setLayout(null);
+			loginButton.setForeground(new Color(0, 0, 0));
 			loginButton.setBackground(SystemColor.textHighlight);
 			loginButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 			loginButton.setFont(new Font("Tahoma", Font.BOLD, 15));
-			loginButton.setBounds(406, 316, 168, 48);
 			panel.add(loginButton);
 			
 			JPasswordField ContrasenaText = new JPasswordField(20);
-			ContrasenaText.setToolTipText("Introduzca su contrase\u00F1a");
+			ContrasenaText.setBounds(79, 226, 434, 7);
+			ContrasenaText.setToolTipText("Introduzca su contraseña");
 			ContrasenaText.setFont(new Font("Tahoma", Font.PLAIN, 15));
 			ContrasenaText.setBorder(new MatteBorder(0, 0, 1, 0, (Color) SystemColor.textHighlight));
 			
 			ContrasenaText.setActionCommand("");
 			ContrasenaText.hide();
-			ContrasenaText.setBounds(79, 191, 434, 42);
 			panel.add(ContrasenaText);
 			
 			UsuarioText = new JTextField();
+			UsuarioText.setBounds(79, 192, 434, 42);
 			UsuarioText.setFont(new Font("Tahoma", Font.PLAIN, 15));
 			UsuarioText.setBorder(new MatteBorder(0, 0, 2, 0, (Color) new Color(180, 180, 180)));
 			UsuarioText.setName("");
-			UsuarioText.setToolTipText("Introduzca su correo electr\u00F3nico");
-			UsuarioText.setBounds(79, 192, 434, 42);
+			UsuarioText.setToolTipText("Introduzca su correo electronico");
 			panel.add(UsuarioText);
 			UsuarioText.setColumns(10);	
 			
 		
 			JLabel lblNewJgoodiesLabel = DefaultComponentFactory.getInstance().createLabel("");
+			lblNewJgoodiesLabel.setBounds(228, 144, 246, 36);
 			lblNewJgoodiesLabel.setIcon(new ImageIcon("C:\\Users\\Usuario\\git\\PROYECTOS_GIT\\TecnoSoftware\\Proyecto-ISO-2-TecnoSoftware\\Proyecto_TecnoSoftware\\Imagenes\\ImagenUCLM.png"));
-			lblNewJgoodiesLabel.setBounds(44, 10, 310, 99);
 			panel.add(lblNewJgoodiesLabel);
 			
-			JButton btnRecuperar = new JButton("He olvidado mi contrase\u00F1a");
+			JButton btnRecuperar = new JButton("He olvidado mi contraseña");
+			btnRecuperar.setBounds(80, 257, 200, 21);
 			btnRecuperar.hide();
 			btnRecuperar.setHorizontalAlignment(SwingConstants.LEFT);
 			btnRecuperar.setForeground(SystemColor.textHighlight);
@@ -121,57 +122,61 @@ public class PantallaLogin  {
 			btnRecuperar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 			btnRecuperar.setBackground(Color.WHITE);
 			btnRecuperar.setBorder(null);
-			btnRecuperar.setBounds(80, 257, 168, 21);
 			panel.add(btnRecuperar);
 
 			frmUclm.setVisible(true);
 			JButton btnSiguiente = new JButton("Siguiente");
+			btnSiguiente.setBounds(406, 316, 168, 48);
 			btnSiguiente.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 			btnSiguiente.setForeground(Color.WHITE);
 			btnSiguiente.setFont(new Font("Tahoma", Font.BOLD, 15));
 			btnSiguiente.setBackground(SystemColor.textHighlight);
-			btnSiguiente.setBounds(406, 316, 168, 48);
 			panel.add(btnSiguiente);
 			
-			JLabel userLabel = new JLabel("Iniciar sesi\u00F3n");
+			JLabel userLabel = new JLabel("Iniciar secion");
+			userLabel.setBounds(25, 11, 148, 25);
 			userLabel.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
 			userLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-			userLabel.setBounds(80, 138, 148, 25);
 			panel.add(userLabel);
 
-			JLabel passwordLabel = new JLabel("Escribir Contrase\u00F1a");
+			JLabel passwordLabel = new JLabel("Contraseña");
+			passwordLabel.setBounds(79, 144, 215, 42);
 			passwordLabel.hide();
 			passwordLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-			passwordLabel.setBounds(80, 129, 215, 42);
 			panel.add(passwordLabel);
 			
 			user = new JTextField();
+			user.setBounds(79, 109, 252, 19);
 			user.setFont(new Font("Tahoma", Font.BOLD, 10));
 			user.setBackground(Color.WHITE);
 			user.setBorder(null);
 			user.setEditable(false);
 			user.hide();
-			user.setBounds(79, 109, 252, 19);
 			panel.add(user);
 			user.setColumns(10);
 			
 			JButton btnNoAcceder = new JButton("\u00BFNo puede acceder a su cuenta?");
+			btnNoAcceder.setBounds(79, 258, 216, 21);
 			btnNoAcceder.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 			btnNoAcceder.setHorizontalAlignment(SwingConstants.LEFT);
 			btnNoAcceder.setForeground(SystemColor.textHighlight);
 			btnNoAcceder.setFont(new Font("Tahoma", Font.BOLD, 12));
 			btnNoAcceder.setBorder(null);
 			btnNoAcceder.setBackground(Color.WHITE);
-			btnNoAcceder.setBounds(79, 258, 216, 21);
 			panel.add(btnNoAcceder);
 			
 			JButton btnNewButton = new JButton("Volver");
+			btnNewButton.setBounds(44, 316, 114, 49);
 			btnNewButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-			btnNewButton.setForeground(Color.WHITE);
+			btnNewButton.setForeground(new Color(0, 0, 0));
 			btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
 			btnNewButton.setBackground(SystemColor.textHighlight);
-			btnNewButton.setBounds(44, 316, 114, 49);
 			panel.add(btnNewButton);
+			
+			JLabel lblUsuario = new JLabel("Usuario:");
+			lblUsuario.setFont(new Font("Tahoma", Font.BOLD, 20));
+			lblUsuario.setBounds(79, 71, 181, 42);
+			panel.add(lblUsuario);
 			btnNewButton.hide();
 			btnNewButton.addActionListener((ActionListener) new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
@@ -211,7 +216,7 @@ public class PantallaLogin  {
 					public void actionPerformed(ActionEvent e) {
 						String pass =ContrasenaText.getText();
 						if (pass.length()==0) {
-							JOptionPane.showMessageDialog(null, "Debe introducir su contrase�a.", "ERROR", JOptionPane.ERROR_MESSAGE);
+							JOptionPane.showMessageDialog(null, "Debe introducir su contraseña.", "ERROR", JOptionPane.ERROR_MESSAGE);
 						}
 						else {
 									 try {
