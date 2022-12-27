bf3dfc71274b1c7aacca69d8e9e3c1fc7730dc31
Samuel-Prog-CSec <115174047+Samuel-Prog-CSec@users.noreply.github.com>
2022-12-27 05:55:23 +0100

    Create MatriculaDAO.java

---
+++ Proyecto_TecnoSoftware/src/persistencia/MatriculaDAO.java
@@ -0,0 +1,31 @@
+package persistencia;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Vector;
+
+import negocio.entities.*;
+
+public class MatriculaDAO {
+	private GestorBD agenteBD;
+
+	public MatriculaDAO() {
+		this.agenteBD = this.agenteBD.getAgente();
+	}
+	
+	public Vector<Object> selectMatriculas(String sql) throws Exception{
+		return agenteBD.select(sql);
+	}
+	
+	public int insertMatricula(String sql) {
+		return agenteBD.insert(sql);
+	}
+
+	public int updateMatricula(String sql) throws SQLException, Exception {
+		return agenteBD.update(sql);
+	}
+	
+	public int deleteMatricula(String sql) {
+		return agenteBD.delete(sql);
+	}
+}
