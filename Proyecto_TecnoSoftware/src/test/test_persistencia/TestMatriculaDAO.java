21b23a4761bddf0be915d202ff04457d74110d88
Pedro Alcázar Barba <pedro.alcazar1@alu.uclm.es>
2022-12-28 22:43:57 +0100

    Creación de carpetas

---
+++ Proyecto_TecnoSoftware/src/test/test_persistencia/TestMatriculaDAO.java
@@ -0,0 +1,14 @@
+package test_persistencia;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestMatriculaDAO {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
