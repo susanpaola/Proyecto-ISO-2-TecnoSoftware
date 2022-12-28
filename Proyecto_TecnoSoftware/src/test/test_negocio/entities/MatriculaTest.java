8f2cabeafdd61441ab2fc940ed9a615dd6333d95
Pedro Alcázar Barba <pedro.alcazar1@alu.uclm.es>
2022-12-28 20:11:24 +0100

    Cambio de ubicación carpeta test

---
+++ Proyecto_TecnoSoftware/src/test/test_negocio/entities/MatriculaTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class MatriculaTest {
+
+}
