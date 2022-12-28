b025e1880d7e22e3a074f125a259a533f1fc0fe8
ivaann17 <ivan.munoz3@alu.uclm.es>
2022-12-28 16:26:48 +0100

    PROBANDO ACTIONS

---
+++ Proyecto_TecnoSoftware/src/presentacion/PRUEBA.java
@@ -0,0 +1,16 @@
+package presentacion;
+
+public class PRUEBA {
+
+	public static void main(String[] args) {
+	System.out.println("hola mundo ");
+	System.out.println("hola mundo ");
+	System.out.println("hola mundo ");
+	System.out.println("hola mundo ");
+	System.out.println("hola mundo ");
+	System.out.println("hola mundo ");
+	System.out.println("hola mundo ");
+	
+	}
+
+}
