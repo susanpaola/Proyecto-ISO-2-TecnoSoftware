075d572b0e2e6d145a2af8dbd6d5f000cb4d476c
Samuel-Prog-CSec <115174047+Samuel-Prog-CSec@users.noreply.github.com>
2022-12-28 07:49:49 +0100

    GestorConsultas terminado

--- Proyecto_TecnoSoftware/src/negocio/controllers/GestorConsultas.java
+++ Proyecto_TecnoSoftware/src/negocio/controllers/GestorConsultas.java
@@ -124,14 +124,88 @@ public class GestorConsultas {
 	 * @param fechaInicio
 	 * @param fechaFin
 	 */
-	public List<CursoPropio> listarEdicionesCursos(Date fechaInicio, Date fechaFin) {
-		// TODO - implement GestorConsultas.listarEdicionesCursos
+	public List<CursoPropio> listarEdicionesCursos(String fechaInicio, String fechaFin) {
 		List<CursoPropio> listaEdicionesCurso = new ArrayList<>();
-		CursoPropioDAO<CursoPropio> curso = new CursoPropioDAO<>();
+		CursoPropioDAO<CursoPropio> cursoDAO = new CursoPropioDAO<>();
 		
-		listaEdicionesCurso = curso.listarEdicionesCurso(fechaInicio, fechaFin);
-		return listaEdicionesCurso;
-		//throw new UnsupportedOperationException();
+		String charsToRemove = "[] ";
+		String sql = "SELECT * FROM CursoPropio WHERE fechaIni >='" + fechaInicio + "' AND fechaFin <='" + fechaFin + "'";
+		Vector<Object> vectorCursos = new Vector<Object>();
+		vectorCursos = cursoDAO.seleccionarCursos(sql);
+		
+		for (int i = 0; i < vectorCursos.size(); i++) {
+			String curso = vectorCursos.get(i).toString();
+				 
+		       	for (char c : charsToRemove.toCharArray()) {
+		        	curso = curso.replace(String.valueOf(c), "");
+			}
+		       
+		        String[] parts = curso.split(",");
+			
+			int id = Integer.parseInt(parts[0]);
+			String nombre = parts[1];
+			int ECTS = Integer.parseInt(parts[2]);
+			String fechaIni = parts[3];
+			String fechaFin = parts[4];
+			double tasa = Double.parseDouble(parts[5]);
+			int edicion = Integer.parseInt(parts[6]);
+			String centro = parts[7];
+			String director = parts[8];
+			String secretario = parts[9];
+			String estadoCurso = parts[10];
+			String tipoCurso = parts[11];
+			
+			EstadoCurso estado = null;
+			switch(estadoCurso) {
+				case "PROPUESTO":
+					estado = EstadoCurso.PROPUESTO;
+					break;
+				case "VALIDADO":
+					estado = EstadoCurso.VALIDADO;
+					break;
+				case "PROPUESTA_RECHAZADA":
+					estado = EstadoCurso.PROPUESTA_RECHAZADA;
+					break;
+				case "EN_MATRICULACION":
+					estado = EstadoCurso.EN_MATRICULACION;
+					break;
+				case "EN_IMPARTIZICION":
+					estado = EstadoCurso.EN_IMPARTIZICION;
+					break;
+				case "TERMINADO":
+					estado = EstadoCurso.TERMINADO;
+					break;
+			}
+			
+			TipoCurso tipo = null;
+			switch(tipoCurso) {
+				case "MASTER":
+					tipo = TipoCurso.MASTER;
+					break;
+				case "EXPERTO":
+					tipo = TipoCurso.EXPERTO;
+					break;
+				case "ESPECIALISTA":
+					tipo = TipoCurso.ESPECIALISTA;
+					break;
+				case "FORMACION_AVANZADA":
+					tipo = TipoCurso.FORMACION_AVANZADA;
+					break;
+				case "FORMACION_AVANZADA":
+					tipo = TipoCurso.FORMACION_AVANZADA;
+					break;
+				case "MICROCREDENCIALES":
+					tipo = TipoCurso.MICROCREDENCIALES;
+					break;
+				case "CORTA_DURACION":
+					tipo = TipoCurso.CORTA_DURACION;
+					break;
+			}
+			
+			CursoPropio cursoCreado = new CursoPropio(id, nombre, ECTS, fechaIni, fechaFin, tasa, edicion, centro, director, secretario, estado, tipo);
+			listaEstadoCurso.add(cursoCreado);
+		}
+		
 	}
 
 }
