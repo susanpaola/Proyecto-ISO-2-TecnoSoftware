239457b41e62d6d580ffa78a816d1ed1e5c27614
ivaann17 <ivan.munoz3@alu.uclm.es>
2022-12-28 17:33:50 +0100

    PROBAR ACTIONS

--- Proyecto_TecnoSoftware/src/presentacion/P.java
+++ Proyecto_TecnoSoftware/src/presentacion/P.java
@@ -4,7 +4,11 @@ public class P {
 
 	public static void main(String[] args) {
 System.out.println("HOLA MUNDO");
-
+System.out.println("HOLA MUNDO");
+System.out.println("HOLA MUNDO");
+System.out.println("HOLA MUNDO");
+System.out.println("HOLA MUNDO");
+System.out.println("HOLA MUNDO");
 	}
 
 }
