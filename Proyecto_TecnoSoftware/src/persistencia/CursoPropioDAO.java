8fb0e150bf8ff8c0ef1fa5d1900ad4dff84ca3ca
Samuel-Prog-CSec <115174047+Samuel-Prog-CSec@users.noreply.github.com>
2022-12-28 18:19:57 +0100

    Update CursoPropioDAO.java

--- Proyecto_TecnoSoftware/src/persistencia/CursoPropioDAO.java
+++ Proyecto_TecnoSoftware/src/persistencia/CursoPropioDAO.java
@@ -26,61 +26,13 @@ public class CursoPropioDAO {
 		return agenteBD.insert(sql);
 	}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	public Vector<Object> seleccionarCursos(String sql) throws Exception {
 		Vector<Object> cursos;
-			cursos = GestorBD.getAgente().select(sql);			
-			return cursos;
-=======
-	public Vector<Object> seleccionarCursos(String sql) {
-		return agenteBD.select(sql);
->>>>>>> refs/heads/Feature-GestorConsultas
-=======
-	public Vector<Object> seleccionarCursos(String sql) {
-		return agenteBD.select(sql);
->>>>>>> refs/heads/Feature-GestorPropuestasCursos
-	}
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-	public Vector<Object> estadoResuelto() throws Exception {
-		Vector<Object> a = seleccionarCursos("SELECT id,nombre,centro,tipo,estado FROM CursoPropio WHERE `estado`<>'PROPUESTO'");
-	return a;
-=======
-	public int editarCurso(String sql) {
-		return agenteBD.update(sql);
->>>>>>> refs/heads/Feature-GestorPropuestasCursos
-	}
-<<<<<<< HEAD
-	public Vector<Object> estadoPendiente() throws Exception {
-		Vector<Object> a = seleccionarCursos("SELECT id,nombre,centro,tipo,estado FROM CursoPropio WHERE `estado`='PROPUESTO'");
-	return a;
-	}
-	public int editarCurso(String sql) throws SQLException, Exception {
-=======
-	public int editarCurso(String sql) {
->>>>>>> refs/heads/Feature-GestorConsultas
-		return agenteBD.update(sql);
-	}
-<<<<<<< HEAD
-
-	//Dado un estado, devolver un Vector<Object> con ellos
-	/*public Vector<Object> listarCursosPorEstado(EstadoCurso estado) throws Exception {
-	return agenteBD.select(sql);
-	}*/
-
-	//Dado un tipo de curso, devolver un double con la suma de sus campos tasa
-	public double listarIngresos(TipoCurso tipo, Date fechaInicio, Date fechaFin) {
-		return 0.00;
+		cursos = GestorBD.getAgente().select(sql);			
+		return cursos;
 	}
 	
-	//Devolver una EDA con los ids de las ediciones
-	public void listarEdicionesCursos(Date fechaInicio, Date fechaFin) {
-		
+	public int editarCurso(String sql) {
+		return agenteBD.update(sql);
 	}
-=======
->>>>>>> refs/heads/Feature-GestorConsultas
-=======
->>>>>>> refs/heads/Feature-GestorPropuestasCursos
 }
