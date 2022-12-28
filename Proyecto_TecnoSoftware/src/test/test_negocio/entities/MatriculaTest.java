34c1de687a1e7d2f5dc45abbc22e48664a43a454
Pedro Alcázar Barba <pedro.alcazar1@alu.uclm.es>
2022-12-28 22:42:57 +0100

    Creación de carpetas

--- Proyecto_TecnoSoftware/src/test/test_negocio/entities/MatriculaTest.java
+++ Proyecto_TecnoSoftware/src/test/test_negocio/entities/MatriculaTest.java
@@ -1,12 +1,14 @@
+package test_negocio.entities;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class MatriculaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
 
 }
