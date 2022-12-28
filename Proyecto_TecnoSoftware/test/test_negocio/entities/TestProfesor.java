cc5cb57ab9a68c9d0a89fd55fa1086e3adcc250d
susan <susanpaola.herrera@alu.uclm.es>
2022-12-28 19:58:36 +0100

    creacion test entidades

---
+++ Proyecto_TecnoSoftware/test/test_negocio/entities/TestProfesor.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestProfesor {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Proyecto_TecnoSoftware/test/test_negocio/entities/TestProfesorExterno.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestProfesorExterno {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
