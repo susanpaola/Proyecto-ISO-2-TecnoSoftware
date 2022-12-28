f0c7064eb0a40c6f8ac93093a49953691ec89b36
Samuel-Prog-CSec <115174047+Samuel-Prog-CSec@users.noreply.github.com>
2022-12-28 04:26:31 +0100

    Update GestorBD.java

--- Proyecto_TecnoSoftware/src/persistencia/GestorBD.java
+++ Proyecto_TecnoSoftware/src/persistencia/GestorBD.java
@@ -1,125 +1,114 @@
 package persistencia;
 
-import java.sql.Connection;
-
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Vector;
 
-
-
-
-
-	public class GestorBD {
-		// instancia del agente
-		protected static GestorBD mInstancia = null;
-		// Conexion con la base de datos
-		protected static Connection mBD;
-		// Identificador ODBC de la base de datos
-		private static String url = "jdbc:mysql://127.0.0.1:3306/TecnoSoftware?user=root";
-		// Driven para conectar con bases de datos MySQL
+public class GestorBD {
+	//Instancia 
+	protected static GestorBD mInstancia = null;
+	// Conexion con la base de datos
+	protected static Connection mBD;
+	// Identificador ODBC de la base de datos
+	private static String url = "jdbc:mysql://db4free.net:3306/proyecto_iso";
+	// Driven para conectar con bases de datos MySQL
 		
-		private static String driver= "com.mysql.cj.jdbc.Driver";
-		private static String user= "root";
-		private static String password="admin";
+	private static String driver= "com.mysql.cj.jdbc.Driver";
+	private static String user= "proyecto_iso";
+	private static String password="contrase\u00f1a";
 	
 		
-		public static void conectar() throws Exception {
+	public static void connect() {
+		try {
 			Class.forName(driver);
 			mBD = DriverManager.getConnection(url, user, password);
 			mBD.setAutoCommit(true);
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-		public static void desconectar() throws Exception {
-			mBD.close();
-		}
-		public static void main(String[] args) throws Exception {
-			conectar();
-			
-			
-			
-		}
-
-		public static GestorBD getmInstancia() {
-			return mInstancia;
-		}
-
-		public static void setmInstancia(GestorBD mInstancia) {
-			GestorBD.mInstancia = mInstancia;
-		}
+	}
+		
+	public static void disconnect() throws Exception {
+		mBD.close();
+	}
 
 
-		public int insert(String SQL) throws SQLException, Exception {
-			conectar();
+	public  int insert(String SQL) {
+		int res = -1;
+		try {
+			connect();
 			PreparedStatement stmt = mBD.prepareStatement(SQL);
-			int res = stmt.executeUpdate(SQL);
+			res = stmt.executeUpdate(SQL);
 			stmt.close();
-			desconectar();
-			return res;
-		}
+			disconnect();
+		} catch (Exception e) {
+			System.out.println(e);
+		} 
+		return res;
+	}
 
-
-		public int delete(String SQL) throws SQLException, Exception {
-			conectar();
+	public int delete(String SQL) {
+		int res = -1;
+		try {
+			connect();
 			PreparedStatement stmt = mBD.prepareStatement(SQL);
-			int res = stmt.executeUpdate(SQL);
+			res = stmt.executeUpdate(SQL);
 			stmt.close();
-			desconectar();
-			return res;
-		}
+			disconnect();
+		} catch (Exception e) {
+			System.out.println(e);
+		} 
+		return res;
+	}
 
-		// Metodo para realizar una eliminacion en la base de datos
-		public int update(String SQL) throws SQLException, Exception {
-			conectar();
-			PreparedStatement stmt = mBD.prepareStatement(SQL);
-			int res = stmt.executeUpdate(SQL);
-			stmt.close();
-			desconectar();
-			return res;
-		}
+	// Metodo para realizar una eliminacion en la base de datos
+	public int update(String SQL) throws SQLException, Exception {
+		connect();
+		PreparedStatement stmt = mBD.prepareStatement(SQL);
+		int res = stmt.executeUpdate(SQL);
+		stmt.close();
+		disconnect();
+		return res;
+	}
 
-
-		public Vector<Object>  select(String SQL) throws Exception {
+	public Vector<Object>  select(String SQL) throws Exception {
 			/*
 			 * Metodo para realizar una busqueda o seleccion de informacion enla base de
-			 * datos El método select develve un vector de vectores, donde cada uno de los
+			 * datos El m≈Ωtodo select develve un vector de vectores, donde cada uno de los
 			 * vectores que contiene el vector principal representa los registros que se
 			 * recuperan de la base de datos.
 			*/
-			Vector<Object> vectoradevolver = new Vector<Object>();
-			conectar();
-			Statement stmt = mBD.createStatement();
-			ResultSet res = stmt.executeQuery(SQL);
-			while (res.next()) {
-				Vector<Object> v = new Vector<Object>();
-				for(int i=1; i<20; i++) {
-					try {
-						v.add(res.getObject(i));
-					}
-					catch(SQLException ex) {
-						continue;
-					}
+		Vector<Object> vectoradevolver = new Vector<Object>();
+		connect();
+		Statement stmt = mBD.createStatement();
+		ResultSet res = stmt.executeQuery(SQL);
+		ResultSetMetaData rsmd = res.getMetaData();
+		int columns = rsmd.getColumnCount();
+		
+		while (res.next()) {
+			Vector<Object> v = new Vector<Object>();
+			for(int i=1; i<=columns; i++) {
+				try {
+					v.add(res.getObject(i));
 				}
-				vectoradevolver.add(v);
+				catch(SQLException ex) {
+					continue;
+				}
 			}
-			stmt.close();
-			conectar();
-			return vectoradevolver;
-
+			vectoradevolver.add(v);
 		}
-
-		public static GestorBD getAgente() throws Exception {
-			if (mInstancia == null) {
-				mInstancia = new GestorBD();
-			}
-			return mInstancia;
-		}
-
-		public GestorBD() throws Exception {
-			conectar();
-		}
-
+		stmt.close();
+		disconnect();
+		return vectoradevolver;
 	}
+
+	public static GestorBD getAgente() throws Exception {
+		if (mInstancia == null) {
+			mInstancia = new GestorBD();
+		}
+		return mInstancia;
+	}
+
+	public GestorBD() throws Exception {
+		connect();
+	}
+}
