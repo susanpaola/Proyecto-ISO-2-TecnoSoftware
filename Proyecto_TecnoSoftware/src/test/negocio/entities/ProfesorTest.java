e1283b458b91ec020f14d946a5a32bdadae4baae
susan <susanpaola.herrera@alu.uclm.es>
2022-12-29 03:21:59 +0100

    update todas entities testing

---
+++ Proyecto_TecnoSoftware/src/test/negocio/entities/ProfesorTest.java
@@ -0,0 +1,24 @@
+package test.negocio.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProfesorTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
