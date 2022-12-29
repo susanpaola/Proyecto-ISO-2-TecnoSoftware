cdd10f9de3adc1d503b78e6386160e0cb152f72f
Pedro Alcázar Barba <pedro.alcazar1@alu.uclm.es>
2022-12-29 02:50:03 +0100

    testGestorMatriculacion añadido

---
+++ Proyecto_TecnoSoftware/testGestorMatriculacion.java
@@ -0,0 +1,15 @@
+package test.negocio.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+
+class testGestorMatriculacion {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
