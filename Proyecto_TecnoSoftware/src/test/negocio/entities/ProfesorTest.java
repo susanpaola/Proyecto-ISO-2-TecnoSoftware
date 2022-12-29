157a34551d91be4e823e885e698ecc3894b14610
susan <susanpaola.herrera@alu.uclm.es>
2022-12-29 03:32:28 +0100

    testing correcto Profesor

--- Proyecto_TecnoSoftware/src/test/negocio/entities/ProfesorTest.java
+++ Proyecto_TecnoSoftware/src/test/negocio/entities/ProfesorTest.java
@@ -2,23 +2,58 @@ package test.negocio.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import main.negocio.entities.Centro;
+import main.negocio.entities.Profesor;
+
 class ProfesorTest {
 
-	@BeforeEach
-	void setUp() throws Exception {
+	Profesor profesor = new Profesor("25895175N", "Sergio", "Onís Gómez", false);
+
+	@Before
+	public void setUp() throws Exception {
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
+	@After
+	public void tearDown() throws Exception {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testGetnombre() {
+		String expected = "Sergio";
+		String actual = profesor.getNombre();
+		assertEquals(expected, actual);
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetlocalizacion() {
+		String expected = "25895175N";
+		String actual = profesor.getDni();
+		assertEquals(expected, actual);
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetattribute() {
+		String expected = "Onís Gómez";
+		String actual = profesor.getApellidos();
+		assertEquals(expected, actual);
+		//assertEquals(expected, actual, 0.0001);
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetdoctor() {
+		boolean expected = false;
+		boolean actual = profesor.isDoctor();
+		assertEquals(expected, actual);
+		//fail("Not yet implemented");
 	}
 
 }
