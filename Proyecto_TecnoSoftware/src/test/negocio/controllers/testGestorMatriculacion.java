8a5d1186798ae2b9936f8cc61b6c182d6a6483b8
Samuel-Prog-CSec <115174047+Samuel-Prog-CSec@users.noreply.github.com>
2022-12-29 04:27:09 +0100

    Update testGestorMatriculacion.java

--- Proyecto_TecnoSoftware/src/test/negocio/controllers/testGestorMatriculacion.java
+++ Proyecto_TecnoSoftware/src/test/negocio/controllers/testGestorMatriculacion.java
@@ -1,31 +1,21 @@
 package test.negocio.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-import java.util.List;
 import java.util.Vector;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 import main.negocio.controllers.GestorMatriculacion;
-import main.negocio.entities.CursoPropio;
-import main.negocio.entities.Estudiante;
 import main.negocio.entities.Matricula;
 import main.negocio.entities.ModoPago;
 import main.persistencia.MatriculaDAO;
 
-class testGestorMatriculacion {
-
+public class testGestorMatriculacion {
 	@Test
-	public void testRealizarMatriculacion() throws Exception {
+	public void realizarMatriculacionTest() {
 		MatriculaDAO agenteMatriculaDAO = new MatriculaDAO();
-		Matricula matricula = new Matricula(3, "2022-12-28", true, 15, ModoPago.TARJETA_CREDITO, 2, "04258756F");
+		Matricula matricula = new Matricula(1000, "2022-12-28", false, 15, ModoPago.TRANSFERENCIA, 3, "04258756F");
 		GestorMatriculacion gestor = new GestorMatriculacion();
 
 		String sql = "SELECT * FROM Matricula";
@@ -36,14 +26,46 @@ class testGestorMatriculacion {
 		Vector<Object> listarMatriculasDespues = agenteMatriculaDAO.selectMatriculas(sql);
 		assertTrue(listarMatriculasDespues.size() == listarMatriculasAntes.size() + 1);
 
-		sql = "DELETE FROM Matricula WHERE id=3";
+		sql = "DELETE FROM Matricula WHERE id=1000";
 		agenteMatriculaDAO.deleteMatricula(sql);
 		
 		} catch(Exception e) {
 			System.out.println(e);
 		}
-	
-
 	}
-
+	
+	@Test
+	public void realizarPagoTest() {
+		MatriculaDAO agenteMatriculaDAO = new MatriculaDAO();
+		Matricula matricula = new Matricula(1001, "2022-12-28", false, 15, ModoPago.TRANSFERENCIA, 3, "04258756F");
+		GestorMatriculacion gestor = new GestorMatriculacion();
+		
+		String sql = null;
+		try {
+			gestor.realizarMatriculacion(matricula);
+			matricula.setPagado(true);
+			gestor.realizarPago(matricula);
+			
+			sql = "SELECT pagado FROM Matricula WHERE id=1001";
+			
+			Vector<Object> matriculaModificada = agenteMatriculaDAO.selectMatriculas(sql);
+			
+			String nuevoPagoCadena = matriculaModificada.get(0).toString();
+			String charsToRemove = "[] ";
+			
+			for (char c : charsToRemove.toCharArray()) {
+				nuevoPagoCadena = nuevoPagoCadena.replace(String.valueOf(c), "");
+	       	}
+			
+			boolean nuevoPago = Boolean.parseBoolean(nuevoPagoCadena);
+			
+			assertTrue(true==nuevoPago);
+			
+			sql = "DELETE FROM Matricula WHERE id=1001";
+			agenteMatriculaDAO.deleteMatricula(sql);
+			
+			} catch(Exception e) {
+				System.out.println(e);
+			}
+	}
 }
