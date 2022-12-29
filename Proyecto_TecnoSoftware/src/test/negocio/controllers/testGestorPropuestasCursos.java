0b15b61d0689ee9bb1a2647915f4bd8d4b98a49c
Samuel-Prog-CSec <115174047+Samuel-Prog-CSec@users.noreply.github.com>
2022-12-29 03:46:41 +0100

    test gestorPropuestasCursos terminado y correcto

--- Proyecto_TecnoSoftware/src/test/negocio/controllers/testGestorPropuestasCursos.java
+++ Proyecto_TecnoSoftware/src/test/negocio/controllers/testGestorPropuestasCursos.java
@@ -80,18 +80,24 @@ class testGestorPropuestasCursos {
 		}
 	}
 	
-	/*
+	
 	@Test
 	public void evaluarPropuestaTest() {
 		CursoPropioDAO agenteCursoPropioDAO = new CursoPropioDAO();
 		EstadoCurso estado = EstadoCurso.PROPUESTA_RECHAZADA;
 		TipoCurso tipo = TipoCurso.FORMACION_AVANZADA;
-		CursoPropio curso = new CursoPropio(101, "Curso_Test", 100, "2022-12-20", "2022-12-29", 900.0, 1, "Facultad de ciencias sociales", "76943196M", "25895175N", estado, tipo);
+		CursoPropio curso = new CursoPropio(102, "Curso_Test", 100, "2022-12-20", "2022-12-29", 900.0, 1, "Facultad de ciencias sociales", "76943196M", "25895175N", estado, tipo);
 		
 		GestorPropuestasCursos gestor = new GestorPropuestasCursos();
 		String sql = null;
 		try {
+			gestor.realizarPropuestaCurso(curso);
+			EstadoCurso estadoCurso = gestor.evaluarPropuesta(curso);
 			
+			int idCurso = curso.getId();
+			String estadoCadena = estado.name();
+			
+			assertTrue(estadoCadena.equals(estadoCurso.name()));
 			
 			sql = "DELETE FROM CursoPropio WHERE nombre='Curso_Test'";
 			agenteCursoPropioDAO.eliminarCurso(sql);
@@ -99,28 +105,38 @@ class testGestorPropuestasCursos {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
 	}
-	*/
 	
 	@Test
-	public void testRealizarMatriculacion() throws Exception {
-		MatriculaDAO agenteMatriculaDAO = new MatriculaDAO();
-		Matricula matricula = new Matricula(1000, "2022-12-28", true, 15, ModoPago.TARJETA_CREDITO, 2, "04258756F");
-		GestorMatriculacion gestor = new GestorMatriculacion();
-
-		String sql = "SELECT * FROM Matricula";
-		try {
-		Vector<Object> listarMatriculasAntes = agenteMatriculaDAO.selectMatriculas(sql);
-		gestor.realizarMatriculacion(matricula);
-
-		Vector<Object> listarMatriculasDespues = agenteMatriculaDAO.selectMatriculas(sql);
-		assertTrue(listarMatriculasDespues.size() == listarMatriculasAntes.size() + 1);
-
-		sql = "DELETE FROM Matricula WHERE id=1000";
-		agenteMatriculaDAO.deleteMatricula(sql);
+	public void altaCursoAprobadoTest() {
+		CursoPropioDAO agenteCursoPropioDAO = new CursoPropioDAO();
+		EstadoCurso estado = EstadoCurso.PROPUESTA_RECHAZADA;
+		TipoCurso tipo = TipoCurso.FORMACION_AVANZADA;
+		CursoPropio curso = new CursoPropio(103, "Curso_Test", 100, "2022-12-20", "2022-12-29", 900.0, 1, "Facultad de ciencias sociales", "76943196M", "25895175N", estado, tipo);
 		
-		} catch(Exception e) {
+		GestorPropuestasCursos gestor = new GestorPropuestasCursos();
+		String sql = null;
+		try {
+			gestor.realizarPropuestaCurso(curso);
+			gestor.altaCursoAprobado(curso);
+			
+			sql = "SELECT estado FROM CursoPropio WHERE nombre='Curso_Test'";
+			
+			Vector<Object> cursoModificado = agenteCursoPropioDAO.seleccionarCursos(sql);
+			
+			String nuevoEstadoCadena = cursoModificado.get(0).toString();
+			String charsToRemove = "[] ";
+			
+			for (char c : charsToRemove.toCharArray()) {
+				nuevoEstadoCadena = nuevoEstadoCadena.replace(String.valueOf(c), "");
+	       	}
+			
+			assertTrue(nuevoEstadoCadena.equals("VALIDADO"));
+			
+			sql = "DELETE FROM CursoPropio WHERE nombre='Curso_Test'";
+			agenteCursoPropioDAO.eliminarCurso(sql);
+		
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
