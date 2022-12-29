c0be88c1f526894022d793b084ab4d0d5634563c
samue <samue@DESKTOP-RRF4FJR>
2022-12-29 05:46:58 +0100

    DAOs testing

---
+++ Proyecto_TecnoSoftware/src/test/persistencia/testCursoPropioDAO.java
@@ -0,0 +1,14 @@
+package test.persistencia;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testCursoPropioDAO {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Proyecto_TecnoSoftware/src/test/persistencia/testMatriculaDAO.java
@@ -0,0 +1,14 @@
+package test.persistencia;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testMatriculaDAO {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
